c7e0c59cb2ec7e6d07d65e1a29b490f2aa5f09e5
Chris Lawson <lawson@avive.life>
2021-04-22 13:14:00 -0500

    Update cleaning scripts so 1 TAB = 8 spaces. This is the Linux way.
    I've fought it long enough. Linus wins. Changing my tools to 8 (from 4).
    
    Linux code lines up better if your editor sets 1 TAB to 8 spaces.
    	Set this for git gui, gitk, and diff tools also.
    
    Linux kernel work is always done with TABs (not spaces).
    	I've been doing a lot of Embedded Linux work lately.
    	Thus, most of the code and scripts I touch use tabs.
    
    For any code that doesn't conform, just run $./convert_to_tabs.sh
    	Be sure to check for unintended side effects before committing.
    	i.e. "git diff"

--- formatting/convert_to_space.sh
+++ formatting/convert_to_space.sh
@@ -3,12 +3,12 @@
 # Note: If you're missing "sponge" run this command:
 # sudo apt install moreutils
 
-# Find all C source and header files. Convert tabs to 4 spaces.
+# Find all C source and header files. Convert tabs to 8 spaces.
 for source_file in `find -type f -name "*.[c,h]"`; do
-	expand -t4 $source_file | sponge $source_file
+	expand -t8 $source_file | sponge $source_file
 done
 
-# Find all shell scripts. Convert tabs to 4 spaces.
+# Find all shell scripts. Convert tabs to 8 spaces.
 for source_file in `find -type f -name "*.sh"`; do
-	expand -t4 $source_file | sponge $source_file
+	expand -t8 $source_file | sponge $source_file
 done
--- formatting/convert_to_tabs.sh
+++ formatting/convert_to_tabs.sh
@@ -3,12 +3,12 @@
 # Note: If you're missing "sponge" run this command:
 # sudo apt install moreutils
 
-# Find all C source and header files. Convert 4 spaces to a tab.
+# Find all C source and header files. Convert 8 spaces to a tab (only at the start of a line).
 for source_file in `find -type f -name "*.[c,h]"`; do
-	unexpand -t4 --first-only $source_file | sponge $source_file
+	unexpand -t8 --first-only $source_file | sponge $source_file
 done
 
-# Find all shell scripts. Convert 4 spaces to a tab.
+# Find all shell scripts. Convert 8 spaces to a tab (only at the start of a line).
 for source_file in `find -type f -name "*.sh"`; do
-	unexpand -t4 --first-only $source_file | sponge $source_file
+	unexpand -t8 --first-only $source_file | sponge $source_file
 done
