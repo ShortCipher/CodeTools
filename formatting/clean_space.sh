0068e18e69e719f5c0e191edddac4dc0fc6717a3
Chris Lawson <lawson@avive.life>
2021-06-21 07:55:40 -0500

    Add debug output to clean_space script

--- formatting/clean_space.sh
+++ formatting/clean_space.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 # Find all C source and header files. Remove trailing whitespace on each line.
+echo ""
+echo "C Source and Header Files (*.c, *.h)"
+echo "------------------------------------"
 find . -type f -name '*.[c,h]' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
 
 # Find all C++ source and header files. Remove trailing whitespace on each line.
+echo ""
+echo "C++ Source and Header Files (*.cpp, *.hpp)"
+echo "------------------------------------------"
 find . -type f -name '*.[c,h]pp' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+
+echo ""
+echo "C++ Source and Header Files (*.cxx, *.hxx)"
+echo "------------------------------------------"
 find . -type f -name '*.[c,h]xx' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
 
 # Find all shell scripts. Remove trailing whitespace on each line.
+echo ""
+echo "Bash Script Files (*.sh)"
+echo "------------------------"
 find . -type f -name '*.sh' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
