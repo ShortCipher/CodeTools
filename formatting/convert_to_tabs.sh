8b1d7a231e54564d5145e8d79608bd9b09d2a582
Chris Lawson <chris@pausingreality.com>
2021-06-21 07:52:48 -0500

    Add C++ files to convert_to_tabs script

--- formatting/convert_to_tabs.sh
+++ formatting/convert_to_tabs.sh
@@ -8,6 +8,15 @@ for source_file in `find . -type f -name "*.[c,h]"`; do
 	unexpand -t8 --first-only $source_file | sponge $source_file
 done
 
+# Find all C++ source and header files. Convert 8 spaces to a tab (only at the start of a line).
+for source_file in `find . -type f -name "*.[c,h]pp"`; do
+	unexpand -t8 --first-only $source_file | sponge $source_file
+done
+
+for source_file in `find . -type f -name "*.[c,h]xx"`; do
+	unexpand -t8 --first-only $source_file | sponge $source_file
+done
+
 # Find all shell scripts. Convert 8 spaces to a tab (only at the start of a line).
 for source_file in `find . -type f -name "*.sh"`; do
 	unexpand -t8 --first-only $source_file | sponge $source_file
