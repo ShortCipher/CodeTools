f6e8beffb145b61613f5cb984bfbaf72238d7efe
Chris Lawson <lawson@avive.life>
2021-06-21 07:39:34 -0500

    Prevent dos2unix from hanging when no files are found

--- formatting/convert_eol.sh
+++ formatting/convert_eol.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
 # Find all C source and header files. Convert line endings to UNIX format.
-dos2unix `find . -type f -name '*.[c,h]'`
+echo ""
+echo "C Source and Header Files (*.c, *.h)"
+echo "------------------------------------"
+for source_file in `find . -type f -name "*.[c,h]"`; do
+	dos2unix -v -S $source_file
+done
 
 # Find all C++ source and header files. Convert line endings to UNIX format.
-dos2unix `find . -type f -name '*.[c,h]pp'`
-dos2unix `find . -type f -name '*.[c,h]xx'`
+echo ""
+echo "C++ Source and Header Files (*.cpp, *.hpp)"
+echo "------------------------------------------"
+for source_file in `find . -type f -name "*.[c,h]pp"`; do
+	dos2unix -v -S $source_file
+done
+
+echo ""
+echo "C++ Source and Header Files (*.cxx, *.hxx)"
+echo "------------------------------------------"
+for source_file in `find . -type f -name "*.[c,h]xx"`; do
+	dos2unix -v -S $source_file
+done
 
 # Find all shell script files. Convert line endings to UNIX format.
-dos2unix `find . -type f -name '*.sh'`
+echo ""
+echo "Bash Script Files (*.sh)"
+echo "------------------------"
+for source_file in `find . -type f -name "*.sh"`; do
+	dos2unix -v -S $source_file
+done
+
+# dos2unix options
+# -v Verbose
+# -S Skip symbolic links
