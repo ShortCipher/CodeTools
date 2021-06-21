70586f80ea11f90aaadcc693ec94ea8a913e66cd
Chris Lawson <chris@pausingreality.com>
2021-06-21 07:50:47 -0500

    Add C++ files to clean_space script

--- formatting/clean_space.sh
+++ formatting/clean_space.sh
@@ -3,5 +3,9 @@
 # Find all C source and header files. Remove trailing whitespace on each line.
 find . -type f -name '*.[c,h]' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
 
+# Find all C++ source and header files. Remove trailing whitespace on each line.
+find . -type f -name '*.[c,h]pp' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+find . -type f -name '*.[c,h]xx' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+
 # Find all shell scripts. Remove trailing whitespace on each line.
 find . -type f -name '*.sh' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
