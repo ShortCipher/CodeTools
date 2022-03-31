e09b83448f0944f639492926b27356140b188a48
Chris Lawson <lawson@avive.life>
2022-03-31 10:15:05 -0500

    Apply ShellCheck recommended fixes (except find -exec)

--- formatting/convert_eol.sh
+++ formatting/convert_eol.sh
@@ -4,31 +4,31 @@
 echo ""
 echo "C Source and Header Files (*.c, *.h)"
 echo "------------------------------------"
-for source_file in `find . -type f -name "*.[c,h]"`; do
-	dos2unix -v -S $source_file
+for source_file in $(find . -type f -name "*.[c,h]"); do
+	dos2unix -v -S "$source_file"
 done
 
 # Find all C++ source and header files. Convert line endings to UNIX format.
 echo ""
 echo "C++ Source and Header Files (*.cpp, *.hpp)"
 echo "------------------------------------------"
-for source_file in `find . -type f -name "*.[c,h]pp"`; do
-	dos2unix -v -S $source_file
+for source_file in $(find . -type f -name "*.[c,h]pp"); do
+	dos2unix -v -S "$source_file"
 done
 
 echo ""
 echo "C++ Source and Header Files (*.cxx, *.hxx)"
 echo "------------------------------------------"
-for source_file in `find . -type f -name "*.[c,h]xx"`; do
-	dos2unix -v -S $source_file
+for source_file in $(find . -type f -name "*.[c,h]xx"); do
+	dos2unix -v -S "$source_file"
 done
 
 # Find all shell script files. Convert line endings to UNIX format.
 echo ""
 echo "Bash Script Files (*.sh)"
 echo "------------------------"
-for source_file in `find . -type f -name "*.sh"`; do
-	dos2unix -v -S $source_file
+for source_file in $(find . -type f -name "*.sh"); do
+	dos2unix -v -S "$source_file"
 done
 
 # dos2unix options
--- formatting/convert_to_space.sh
+++ formatting/convert_to_space.sh
@@ -7,29 +7,29 @@
 echo ""
 echo "C Source and Header Files (*.c, *.h)"
 echo "------------------------------------"
-for source_file in `find . -type f -name "*.[c,h]"`; do
-	expand -t8 $source_file | sponge $source_file
+for source_file in $(find . -type f -name "*.[c,h]"); do
+	expand -t8 "$source_file" | sponge "$source_file"
 done
 
 # Find all C++ source and header files. Convert tabs to 8 spaces.
 echo ""
 echo "C++ Source and Header Files (*.cpp, *.hpp)"
 echo "------------------------------------------"
-for source_file in `find . -type f -name "*.[c,h]pp"`; do
-	expand -t8 $source_file | sponge $source_file
+for source_file in $(find . -type f -name "*.[c,h]pp"); do
+	expand -t8 "$source_file" | sponge "$source_file"
 done
 
 echo ""
 echo "C++ Source and Header Files (*.cxx, *.hxx)"
 echo "------------------------------------------"
-for source_file in `find . -type f -name "*.[c,h]xx"`; do
-	expand -t8 $source_file | sponge $source_file
+for source_file in $(find . -type f -name "*.[c,h]xx"); do
+	expand -t8 "$source_file" | sponge "$source_file"
 done
 
 # Find all shell scripts. Convert tabs to 8 spaces.
 echo ""
 echo "Bash Script Files (*.sh)"
 echo "------------------------"
-for source_file in `find . -type f -name "*.sh"`; do
-	expand -t8 $source_file | sponge $source_file
+for source_file in $(find . -type f -name "*.sh"); do
+	expand -t8 "$source_file" | sponge "$source_file"
 done
