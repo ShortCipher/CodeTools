5840a7030ee8600f6399ba870a88636812daf72c
Chris Lawson <chris@pausingreality.com>
2025-09-12 09:05:29 -0700

    Use -exec to avoid "for" loop
    Also, use -print to show filename(s) on console output.

--- formatting/convert_to_space.sh
+++ formatting/convert_to_space.sh
@@ -7,29 +7,21 @@
 echo ""
 echo "C Source and Header Files (*.c, *.h)"
 echo "------------------------------------"
-for source_file in $(find . -type f -name "*.[c,h]"); do
-	expand -t8 "$source_file" | sponge "$source_file"
-done
+find . -type f -name "*.[c,h]" -print -exec sh -c 'expand -t8 "$1" | sponge "$1"' _ {} \+
 
 # Find all C++ source and header files. Convert tabs to 8 spaces.
 echo ""
 echo "C++ Source and Header Files (*.cpp, *.hpp)"
 echo "------------------------------------------"
-for source_file in $(find . -type f -name "*.[c,h]pp"); do
-	expand -t8 "$source_file" | sponge "$source_file"
-done
+find . -type f -name "*.[c,h]pp" -print -exec sh -c 'expand -t8 "$1" | sponge "$1"' _ {} \+
 
 echo ""
 echo "C++ Source and Header Files (*.cxx, *.hxx)"
 echo "------------------------------------------"
-for source_file in $(find . -type f -name "*.[c,h]xx"); do
-	expand -t8 "$source_file" | sponge "$source_file"
-done
+find . -type f -name "*.[c,h]xx" -print -exec sh -c 'expand -t8 "$1" | sponge "$1"' _ {} \+
 
 # Find all shell scripts. Convert tabs to 8 spaces.
 echo ""
 echo "Bash Script Files (*.sh)"
 echo "------------------------"
-for source_file in $(find . -type f -name "*.sh"); do
-	expand -t8 "$source_file" | sponge "$source_file"
-done
+find . -type f -name "*.sh" -print -exec sh -c 'expand -t8 "$1" | sponge "$1"' _ {} \+
