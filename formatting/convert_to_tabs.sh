1277424c26a70f507bc5fa6e11c2905ebd76a6bf
Chris Lawson <chris@pausingreality.com>
2025-09-12 08:52:52 -0700

    Use -print and -exec for clarity and simplicity

--- formatting/convert_to_tabs.sh
+++ formatting/convert_to_tabs.sh
@@ -7,29 +7,21 @@
 echo ""
 echo "C Source and Header Files (*.c, *.h)"
 echo "------------------------------------"
-for source_file in $(find . -type f -name "*.[c,h]"); do
-	unexpand -t8 --first-only "$source_file" | sponge "$source_file"
-done
+find . -type f -name "*.[ch]" -print -exec sh -c 'unexpand -t8 --first-only "$1" | sponge "$1"' _ {} +
 
 # Find all C++ source and header files. Convert 8 spaces to a tab (only at the start of a line).
 echo ""
 echo "C++ Source and Header Files (*.cpp, *.hpp)"
 echo "------------------------------------------"
-for source_file in $(find . -type f -name "*.[c,h]pp"); do
-	unexpand -t8 --first-only "$source_file" | sponge "$source_file"
-done
+find . -type f -name "*.[c,h]pp" -print -exec sh -c 'unexpand -t8 --first-only "$1" | sponge "$1"' _ {} +
 
 echo ""
 echo "C++ Source and Header Files (*.cxx, *.hxx)"
 echo "------------------------------------------"
-for source_file in $(find . -type f -name "*.[c,h]xx"); do
-	unexpand -t8 --first-only "$source_file" | sponge "$source_file"
-done
+find . -type f -name "*.[c,h]xx" -print -exec sh -c 'unexpand -t8 --first-only "$1" | sponge "$1"' _ {} +
 
 # Find all shell scripts. Convert 8 spaces to a tab (only at the start of a line).
 echo ""
 echo "Bash Script Files (*.sh)"
 echo "------------------------"
-for source_file in $(find . -type f -name "*.sh"); do
-	unexpand -t8 --first-only "$source_file" | sponge "$source_file"
-done
+find . -type f -name "*.sh" -print -exec sh -c 'unexpand -t8 --first-only "$1" | sponge "$1"' _ {} +
