acac7effec49d178e50aad78172639785dd8f33d
Chris Lawson <chris@pausingreality.com>
2025-09-12 08:04:06 -0700

    Use -exec instead of "for" loop for simplicity

--- formatting/convert_eol.sh
+++ formatting/convert_eol.sh
@@ -4,32 +4,24 @@
 echo ""
 echo "C Source and Header Files (*.c, *.h)"
 echo "------------------------------------"
-find . -type f -name "*.[c,h]" | while read -r source_file; do
-	dos2unix -v -S "$source_file"
-done
+find . -type f -name "*.[c,h]" -exec dos2unix -v -S {} +
 
 # Find all C++ source and header files. Convert line endings to UNIX format.
 echo ""
 echo "C++ Source and Header Files (*.cpp, *.hpp)"
 echo "------------------------------------------"
-find . -type f -name "*.[c,h]pp" | while read -r source_file; do
-	dos2unix -v -S "$source_file"
-done
+find . -type f -name "*.[c,h]pp" -exec dos2unix -v -S {} +
 
 echo ""
 echo "C++ Source and Header Files (*.cxx, *.hxx)"
 echo "------------------------------------------"
-find . -type f -name "*.[c,h]xx" | while read -r source_file; do
-	dos2unix -v -S "$source_file"
-done
+find . -type f -name "*.[c,h]xx" -exec dos2unix -v -S {} +
 
 # Find all shell script files. Convert line endings to UNIX format.
 echo ""
 echo "Bash Script Files (*.sh)"
 echo "------------------------"
-find . -type f -name "*.sh" | while read -r source_file; do
-	dos2unix -v -S "$source_file"
-done
+find . -type f -name "*.sh" -exec dos2unix -v -S {} +
 
 # dos2unix options
 # -v Verbose
