68891a218b512155ea96e2f87944830cadd53cab
Chris Lawson <chris@pausingreality.com>
2025-09-12 07:02:43 -0700

    Add -print argument to print filename(s) in console output

--- formatting/clean_space.sh
+++ formatting/clean_space.sh
@@ -4,21 +4,21 @@
 echo ""
 echo "C Source and Header Files (*.c, *.h)"
 echo "------------------------------------"
-find . -type f -name '*.[c,h]' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+find . -type f -name '*.[c,h]' -print -exec sed --in-place 's/[[:space:]]\+$//' {} \+
 
 # Find all C++ source and header files. Remove trailing whitespace on each line.
 echo ""
 echo "C++ Source and Header Files (*.cpp, *.hpp)"
 echo "------------------------------------------"
-find . -type f -name '*.[c,h]pp' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+find . -type f -name '*.[c,h]pp' -print -exec sed --in-place 's/[[:space:]]\+$//' {} \+
 
 echo ""
 echo "C++ Source and Header Files (*.cxx, *.hxx)"
 echo "------------------------------------------"
-find . -type f -name '*.[c,h]xx' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+find . -type f -name '*.[c,h]xx' -print -exec sed --in-place 's/[[:space:]]\+$//' {} \+
 
 # Find all shell scripts. Remove trailing whitespace on each line.
 echo ""
 echo "Bash Script Files (*.sh)"
 echo "------------------------"
-find . -type f -name '*.sh' -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+find . -type f -name '*.sh' -print -exec sed --in-place 's/[[:space:]]\+$//' {} \+
